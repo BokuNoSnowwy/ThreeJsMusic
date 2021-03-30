4df76b914156eac8e5973f2498ff40b25a05ff02
Mornet Valentin <snow855@hotmail.fr>
2021-03-30 10:52:07 +0200

    Camera FPS

--- js/game.js
+++ js/game.js
@@ -1,14 +1,23 @@
 import { FBXLoader } from '../loaders/FBXLoader.js';
+import { OrbitControls } from '../controls/OrbitControls.js';
+import { FirstPersonControls } from '../controls/FirstPersonControls.js';
+import { PointerLockControls } from '../controls/PointerLockControls.js';
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialas: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 const clock = new THREE.Clock();
+
+//First Person Camera
+const controls = new PointerLockControls( camera, renderer.domElement);
+controls.maxPolarAngle = 2;
+controls.minPolarAngle = 1;
+scene.add( controls.getObject() );
 
 //Mesh Import
 const loader = new FBXLoader();
-
-
 
 //Music 
 // create an AudioListener and add it to the camera
@@ -17,13 +26,13 @@ camera.add(listener);
 // create a global audio source
 const sound = new THREE.Audio(listener);
 
-
 //BPM
 const bpm = 128;
 const secondBeat = 1 / (bpm / 60);
 let timerTic = secondBeat;
 var countTic = 1;
 var demiTic;
+
 //MeshArray
 const torusMesh = [];
 const cubeMeshs = [];
@@ -31,7 +40,6 @@ const cubeMeshs = [];
 //Mesh Offsets
 const cubeOffset = new THREE.Vector3(1.08, -0.5, 0.06)
 let visibleMeshs = false;
-
 let ratioCube = 0.025;
 
 //Cube1
@@ -58,10 +66,13 @@ var material = new THREE.MeshStandardMaterial({ color: 0xff0120, flatShading: tr
 let cube4 = new THREE.Mesh(geometry, material);
 cubeMeshs.push(cube4);
 
+//Starting Button
 const startButton = document.getElementById("startButton");
 startButton.addEventListener('click', function () {
     init();
     hideButton();
+    controls.lock();
+    console.log(controls);
 })
 function hideButton() {
     document.getElementById("startButton").style.display = 'none';
@@ -105,7 +116,7 @@ function init() {
         demiTic = false
     })
 
-    renderer.setClearColor("#222222")
+    renderer.setClearColor("#111111")
 
     //Ambient Lights 
     var ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
@@ -138,11 +149,11 @@ function init() {
                 if (child.isMesh) {
                     //Change Purple Mat
                     child.material[1].emissive = child.material[1].color;
-                    child.material[1].emissiveIntensity = 0.30;
+                    child.material[1].emissiveIntensity = 0.50;
 
                     //Change Red Mat
                     child.material[4].emissive = child.material[4].color;
-                    child.material[4].emissiveIntensity = 0.30;
+                    child.material[4].emissiveIntensity = 0.50;
                     //console.log( child.material);
 
                     //Change Black Mat
@@ -159,15 +170,12 @@ function init() {
 
 animate();
 clockTic();
-
-
-
     
 
 //Box Animation
 function animate() {
     requestAnimationFrame(animate);
-
+    controls.getObject().position.set( 0, 0, 2 );
     camera.position.y = 0.15;
     //Torus Rotation
     torusMesh.forEach(torus => {
@@ -178,8 +186,8 @@ function animate() {
             case true:
                 torus.traverse(function (child) {
                     if (child.isMesh) {
-                        child.material[1].emissive = new THREE.Color(0x00ff00);
-                        child.material[1].color = new THREE.Color(0x00ff00);
+                        child.material[1].emissive = new THREE.Color(0x8a2be2);
+                        child.material[1].color = new THREE.Color(0x8a2be2);
                         child.material[4].emissive = new THREE.Color(0xff0000);
                         child.material[4].color = new THREE.Color(0xff0000);
                     }
@@ -190,8 +198,8 @@ function animate() {
                     if (child.isMesh) {
                         child.material[1].emissive = new THREE.Color(0xff0000);
                         child.material[1].color = new THREE.Color(0xff0000);
-                        child.material[4].emissive = new THREE.Color(0x00ff00);
-                        child.material[4].color = new THREE.Color(0x00ff00);
+                        child.material[4].emissive = new THREE.Color(0x8a2be2);
+                        child.material[4].color = new THREE.Color(0x8a2be2);
                     }
                 });
                 break;
@@ -199,8 +207,6 @@ function animate() {
     });
 
    
-        
-
     //All Cube Rotation
     cubeMeshs.forEach(cube => {
         cube.rotation.x += 0.04;
@@ -359,30 +365,12 @@ function cubeCreations(){
             }
         });
     });
-    
-    
-    /*
-    loader.load('meshs/fbx/Cube.fbx', function (object) {
-        scene.add(object);
-
-        setUpMesh(cube4, object, new THREE.Vector3(-1.07, -1, 0))
-        var x = 1 / 10;
-        object.scale.set(x, x, x);
-
-        object.traverse(function (child) {
-            if (child.isMesh) {
-                child.material[1].emissive = new THREE.Color(0xff0000);
-                child.material[1].emissiveIntensity = 1;
-                child.material[1].color = new THREE.Color(0xff0000);;
-            }
-        });
-    });
-    */
 }
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
 
+    /*
     var keyCode = event.which;
 
     if (keyCode == 90) {
@@ -406,4 +394,5 @@ function onDocumentKeyDown(event) {
         scene.add(cube);
         cube.position.set(0, 0, 0);
     }
+    */
 }
